1d08d18d963e444a794096b062adf323b46dd44f
ICMMAC05-7528 <icmmac05-7528@ICMMAC05-7528s-Mac-mini.local>
2022-10-11 18:42:22 -0400

    quiz

---
+++ miniquiz/miniquiz/controller/miniquizViewViewController.swift
@@ -0,0 +1,28 @@
+//
+//  miniquizViewViewController.swift
+//  miniquiz
+//
+//  Created by ICMMAC03-BEBA on 11/10/22.
+//
+
+import UIKit
+
+
+class miniquizViewViewController: UIViewController {
+
+    @IBOutlet var opc: [UIButton]!
+    
+    
+
+    @IBOutlet var bt1: [UIButton]!
+    
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    question.text =
+
+
+}
